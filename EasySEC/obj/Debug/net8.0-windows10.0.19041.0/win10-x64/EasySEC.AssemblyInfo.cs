b0e55f8f2ccf4dd53f182d491b0204043712c036
MegaSusl <Suslik_k@list.ru>
2025-03-17 20:22:22 +0300

    b

--- EasySEC/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/EasySEC.AssemblyInfo.cs
+++ EasySEC/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/EasySEC.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySEC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+91db7478fe3374c98f450df5bd7d62dff5ea3a41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9568bbe03816ca3921608fa7075efb64d1e67fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySEC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySEC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
